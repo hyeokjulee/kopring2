52d02aa3b263b73f19d45b7585e3cd13109f8158
HyeokJu Lee <dlhjwwe@naver.com>
2024-02-28 20:58:55 +0900

    MemberRepositories.kt 생성

---
+++ src/main/kotlin/com/example/demo/member/repositoty/MemberRepositories.kt
@@ -0,0 +1,8 @@
+package com.example.demo.member.repositoty
+
+import com.example.demo.member.entity.Member
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByLoginId(loginId: String): Member?
+}
