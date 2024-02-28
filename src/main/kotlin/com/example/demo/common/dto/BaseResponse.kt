24a96e33c0b1958e88359e814014eff1dab3a76e
HyeokJu Lee <dlhjwwe@naver.com>
2024-02-29 00:59:21 +0900

    InvalidInputException.kt 생성

--- src/main/kotlin/com/example/demo/common/dto/BaseResponse.kt
+++ src/main/kotlin/com/example/demo/common/dto/BaseResponse.kt
@@ -5,5 +5,5 @@ import com.example.demo.common.status.ResultCode
 data class BaseResponse<T>(
     val resultCode: String = ResultCode.SUCCESS.name,
     val data: T? = null,
-    val message: String = ResultCode.SUCCESS.msg,
+    val message: String = ResultCode.SUCCESS.msg
 )
