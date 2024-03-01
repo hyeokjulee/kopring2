829662cbea2311d292fbaf649a327059dc01ff71
HyeokJu Lee <dlhjwwe@naver.com>
2024-03-02 08:17:15 +0900

    MemberService의 signUp() 수정

--- src/main/kotlin/com/example/demo/member/service/MemberService.kt
+++ src/main/kotlin/com/example/demo/member/service/MemberService.kt
@@ -1,16 +1,20 @@
 package com.example.demo.member.service
 
 import com.example.demo.common.exception.InvalidInputException
+import com.example.demo.common.status.ROLE
 import com.example.demo.member.dto.MemberDtoRequest
 import com.example.demo.member.entity.Member
+import com.example.demo.member.entity.MemberRole
 import com.example.demo.member.repositoty.MemberRepository
+import com.example.demo.member.repositoty.MemberRoleRepository
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Transactional
 @Service
 class MemberService(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val memberRoleRepository: MemberRoleRepository
 ) {
     /**
      * 회원가입
@@ -19,6 +23,11 @@ class MemberService(
         if (memberRepository.findByLoginId(memberDtoRequest.loginId) != null) { // ID 중복 검사
             throw InvalidInputException("loginId", "이미 등록된 ID 입니다.")
         }
-        memberRepository.save(memberDtoRequest.toEntity())
+
+        val member = memberDtoRequest.toEntity()
+        memberRepository.save(member)
+
+        val memberRole: MemberRole = MemberRole(null, ROLE.MEMBER, member)
+        memberRoleRepository.save(memberRole)
     }
 }
