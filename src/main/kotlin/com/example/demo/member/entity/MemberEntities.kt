36f8b4412a0f4be42d74b85a9a27ddddd662df5a
HyeokJu Lee <dlhjwwe@naver.com>
2024-03-02 07:02:37 +0900

    MemberEntities.kt에 MemberRole 추가

--- src/main/kotlin/com/example/demo/member/entity/MemberEntities.kt
+++ src/main/kotlin/com/example/demo/member/entity/MemberEntities.kt
@@ -1,6 +1,7 @@
 package com.example.demo.member.entity
 
 import com.example.demo.common.status.Gender
+import com.example.demo.common.status.ROLE
 import jakarta.persistence.*
 import java.time.LocalDate
 
@@ -32,4 +33,22 @@ class Member(
 
     @Column(nullable = false, length = 30)
     val email: String,
+) {
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "member")
+    val memberRole: List<MemberRole>? = null
+}
+
+@Entity
+class MemberRole(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null,
+
+    @Column(nullable = false, length = 30)
+    @Enumerated(EnumType.STRING)
+    var role: ROLE,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = ForeignKey(name = "fk_member_role_member_id"))
+    val member: Member
 )
