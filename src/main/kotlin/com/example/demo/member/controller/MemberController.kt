079286c975cd15971494ba227bb2a05d1f0d63a5
HyeokJu Lee <dlhjwwe@naver.com>
2024-03-02 08:47:35 +0900

    MemberController에 login() 추가

--- src/main/kotlin/com/example/demo/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/demo/member/controller/MemberController.kt
@@ -1,6 +1,8 @@
 package com.example.demo.member.controller
 
+import com.example.demo.common.authority.TokenInfo
 import com.example.demo.common.dto.BaseResponse
+import com.example.demo.member.dto.LoginDto
 import com.example.demo.member.dto.MemberDtoRequest
 import com.example.demo.member.service.MemberService
 import jakarta.validation.Valid
@@ -21,5 +23,14 @@ class MemberController(
     fun signUp(@RequestBody @Valid memberDtoRequest: MemberDtoRequest): BaseResponse<Unit> {
         memberService.signUp(memberDtoRequest)
         return BaseResponse(message = "회원가입이 완료되었습니다.")
+    }
+
+    /**
+     * 로그인
+     */
+    @PostMapping("/login")
+    fun login(@RequestBody @Valid loginDto: LoginDto): BaseResponse<TokenInfo> {
+        val tokenInfo = memberService.login(loginDto)
+        return BaseResponse(data = tokenInfo)
     }
 }
