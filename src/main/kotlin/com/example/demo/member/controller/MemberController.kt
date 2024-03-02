d8f34aa56b36f97ef70f26b53a99aef43ac8408c
HyeokJu Lee <dlhjwwe@naver.com>
2024-03-03 05:35:48 +0900

    MemberController에 saveMyInfo() 추가

--- src/main/kotlin/com/example/demo/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/demo/member/controller/MemberController.kt
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -47,5 +48,16 @@ class MemberController(
         val userId = (SecurityContextHolder.getContext().authentication.principal as CustomUser).userId
         val response = memberService.searchMyInfo(userId)
         return BaseResponse(data = response)
+    }
+
+    /**
+     * 내 정보 보기
+     */
+    @PutMapping("/saveMyInfo")
+    fun saveMyInfo(@RequestBody @Valid memberDtoRequest: MemberDtoRequest): BaseResponse<Unit> {
+        val userId = (SecurityContextHolder.getContext().authentication.principal as CustomUser).userId
+        memberDtoRequest.id = userId
+        memberService.saveMyInfo(memberDtoRequest)
+        return BaseResponse(message = "수정 완료되었습니다.")
     }
 }
